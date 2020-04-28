a482dfd1c6c913539120c1dfbb82117afa156a62
merewelch214 <merewelch@gmail.com>
2020-04-28 15:10:49 -0400

    edit journal entry

--- src/JournalEntry.js
+++ src/JournalEntry.js
@@ -4,8 +4,13 @@ class JournalEntry extends React.Component {
   
   state = {
     entry: '',
-    user_id: 1
+    user_id: 1, 
+    saved: false
   }
+
+  // componentDidMount: grab today's journal entry if it exists. if not, return null and show the form. 
+  // if looking at a past day's journal, do not allow an edit, just show the results.
+  // need to send the date as a parameter to the fetch request.
 
   // save journal entry to user data and reset the form. Once saved, should redirect to dashboard.
   handleSubmit = (e) => {
@@ -16,10 +21,13 @@ class JournalEntry extends React.Component {
         'Accept': 'application/json',
         'Content-type': 'application/json'
       },
-      body: JSON.stringify(this.state)
+      body: JSON.stringify({user_id: this.state.user_id, entry: this.state.entry})
     })
 
-    this.setState({ entry: '' })
+    this.setState({ 
+      entry: '',
+      saved: true
+    })
   }
 
   handleChange = (e) => {
@@ -39,16 +47,26 @@ class JournalEntry extends React.Component {
     
     const today = new Date();
     
+    const showForm = 
+      <form onSubmit={this.handleSubmit} id='entry-form'>
+        <label name='entry'/>
+        <textarea id='entry-input' name='entry' value={this.state.entry} onChange={this.handleChange} placeholder="What are your wildest dreams?"/><br />
+        <button type="submit" value="Submit">Save</button>
+        <button type="reset" onClick={this.clear}>Clear</button>
+      </form>
+
+    const editEntry = 
+      <div>
+        <p>{this.state.entry}</p> 
+        <button type="edit" value="edit">Edit</button>
+      </div>
+
+
     return (
         <div>
           <h1 className="title">{`${moment(today).format('ll')} Journal`}</h1>
           <div className="container">
-              <form onSubmit={this.handleSubmit} id='entry-form'>
-                <label name='entry'/>
-                <textarea id='entry-input' name='entry' value={this.state.entry} onChange={this.handleChange} placeholder="What are your wildest dreams?"/><br />
-                <button type="submit" value="Submit">Save</button>
-                <button type="reset" onClick={this.clear}>Clear</button>
-              </form>
+              { this.state.saved ? editEntry : showForm }
           </div>
         </div> 
       );
