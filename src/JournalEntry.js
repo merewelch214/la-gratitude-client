d377080cc8f8c31a8e8ecf17d693304c7a7deb9c
merewelch214 <merewelch@gmail.com>
2020-04-29 15:59:26 -0400

    journal entry conditional rendering

--- src/JournalEntry.js
+++ src/JournalEntry.js
@@ -4,15 +4,31 @@ class JournalEntry extends React.Component {
   
   state = {
     entry: '',
-    user_id: 1, 
+    user_id: 1,
+    edit_toggle: false,
+    id: '',
     saved: false
   }
 
-  // componentDidMount: grab today's journal entry if it exists. if not, return null and show the form. 
+  // componentDidMount: return today's journal entry if it exists. if not, return an empty string and show the form. 
+  componentDidMount() {
+    fetch(`http://localhost:3000/today_journal_entry`)
+    .then(resp => resp.json())
+    .then(data =>  
+      this.setState({ 
+        entry: data.entry,
+        id: data.id
+      })  
+    )
+  }
+
+  //TO DO:
+  // allow edits to today's posts after submission only.
   // if looking at a past day's journal, do not allow an edit, just show the results.
   // need to send the date as a parameter to the fetch request.
 
-  // save journal entry to user data and reset the form. Once saved, should redirect to dashboard.
+  // save journal entry to user data
+  // TO DO: Once saved, should redirect to dashboard.
   handleSubmit = (e) => {
     e.preventDefault()
     fetch(`http://localhost:3000/journal_entry`, {
@@ -23,52 +39,95 @@ class JournalEntry extends React.Component {
       },
       body: JSON.stringify({user_id: this.state.user_id, entry: this.state.entry})
     })
-
-    this.setState({ 
-      entry: '',
+    .then(resp=> resp.json())
+    .then(data=> this.setState({
+      id: data.id,
       saved: true
-    })
+    }))
   }
 
+  // controlled form: setting the state with new values from the form
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value})
+    this.setState({ 
+      [e.target.name]: e.target.value, 
+      saved: false})
   }
 
+  // clear button will clear prior info in the form
   clear = () => {
     this.setState({
       entry: ''
     })
   }
 
-  // To Do: add function to randomly show inspirational quote in empty textarea.
+  // show the edit form
+  editPost = () => {
+    this.setState({editToggle: true})
+  }
 
+  // send a patch request to update the entry data on the backend.
+  handleEdit = () => {
+    fetch(`http://localhost:3000/journal_entry/${this.state.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({entry: this.state.entry})
+    })
+    this.setState({editToggle: false, saved: true})
+  }
+
+  // delete fetch request. reset state.
+  deletePost = () => {
+    fetch(`http://localhost:3000/journal_entry/${this.state.id}`, {
+      method: 'DELETE'
+    })
+    
+    this.setState({
+      entry: '',
+      id: ''
+    })
+}
+
+  // To Do: add function to randomly show inspirational quote in empty textarea.
 
   render() {  
     
     const today = new Date();
-    
-    const showForm = 
+
+    const entryForm = 
       <form onSubmit={this.handleSubmit} id='entry-form'>
         <label name='entry'/>
         <textarea id='entry-input' name='entry' value={this.state.entry} onChange={this.handleChange} placeholder="What are your wildest dreams?"/><br />
         <button type="submit" value="Submit">Save</button>
-        <button type="reset" onClick={this.clear}>Clear</button>
+        <button type="reset" onClick={this.clear}>Clear</button> 
       </form>
 
-    const editEntry = 
+    const showEntry =
       <div>
-        <p>{this.state.entry}</p> 
-        <button type="edit" value="edit">Edit</button>
+        <p> {this.state.entry} </p>
+        <button type='edit' value='edit' onClick={this.editPost}> Edit </button> 
+        <button type='delete' value='delete' onClick={this.deletePost}> Delete </button> 
       </div>
 
+    const editForm = 
+      <form onSubmit={this.handleEdit} id='entry-form'>
+        <label name='entry'/>
+        <textarea id='entry-input' name='entry' value={this.state.entry} onChange={this.handleChange} placeholder="What are your wildest dreams?"/><br />
+        <button type="submit" value="Submit">Save</button>
+        <button type="reset" onClick={this.clear}>Clear</button> 
+      </form>
 
-    return (
-        <div>
-          <h1 className="title">{`${moment(today).format('ll')} Journal`}</h1>
-          <div className="container">
-              { this.state.saved ? editEntry : showForm }
-          </div>
-        </div> 
+    console.log(this.state)
+    console.log(this.state.edit_toggle)
+      return (
+          <div>
+            <h1 className="title">{`${moment(today).format('ll')} Journal`}</h1>
+            <div className="container">
+
+            </div>
+          </div> 
       );
     }
   }
