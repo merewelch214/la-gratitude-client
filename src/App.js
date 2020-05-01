eec566c56e2d429e68b2b5fd1e289d8d5fb5e939
merewelch214 <merewelch@gmail.com>
2020-05-01 15:57:56 -0400

    setting up initial routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import MainContainer from './MainContainer'
+import { withRouter, Switch, Route } from "react-router-dom";
+import SignUp from './SignUp'
+import Login from './Login'
 
 function App() {
   return (
-    <div className="App">
-        <MainContainer />
-    </div>
+      <div className="App">
+        <Switch>
+            <Route path='/signup' render={() => <SignUp />}/>
+            <Route path='/login' render={() => <Login />}/>
+            <Route path='/journal' render={() => <MainContainer />}/>
+            <Route path='/dashboard' render= {() => <MainContainer />}/>
+            <Route path='/' render={() =>  <MainContainer />}/>  
+          </Switch >
+      </div>
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div>
+            This is where the log in page will go
+        </div>
+    );
+  }
+  
+  export default Login;
