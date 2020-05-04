4b5b7d44d101c4168d30768fc000821986415ba8
merewelch214 <merewelch@gmail.com>
2020-05-04 11:29:52 -0400

    goal form

--- src/Goals.js
+++ src/Goals.js
@@ -1,17 +1,72 @@
 import React from 'react';
 
-function Goals() {
+class Goals extends React.Component {
+  
+  state = {
+    user_id: 1,
+    goals: [],
+    new_goal: ''
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3000/goals`)
+    .then(resp=>resp.json())
+    .then(goals=>this.setState({goals}))
+  }
+
+  addGoal = (e) => {
+    e.preventDefault()
+    fetch(`http://localhost:3000/goal`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({title: this.state.new_goal, user_id: this.state.user_id})
+    })
+    .then(resp=>resp.json())
+    .then(goal=>this.setState((prevState) => ({
+      goals: [...prevState.goals, goal]
+    }))) 
+    this.setState({new_goal: ''})
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({new_goal: e.target.value})
+  }
+
+  handleDelete = (e, id) => {
+    e.preventDefault()
+    fetch(`http://localhost:3000/goal/${id}`, {
+      method: 'DELETE'})
+    let goals = this.state.goals.filter(goal => goal.id !== id)
+    this.setState({goals})
+  }
+
+  render() {
+    let { goals } = this.state
     return (
-      <React.Fragment>
-        <h1 className="title">Goals</h1>
-        <div className="container small">
-            <p className='small-description'>Use this space to add your goals for today</p>
-            <form> 
-              <input type='text' className='new-item'/> <button className='add'> + </button>
-            </form>
-        </div>
-      </React.Fragment>
-    );
+        <React.Fragment>
+          <h1 className="title">Goals</h1>
+          <div className="container small">
+              <p className='small-description'>Use this space to add your goals for today</p>
+              <form> 
+                {goals.map(goal => 
+                      <div key={goal.id}>
+                        <input readOnly type='text'
+                          value={goal.title} 
+                          className='new-item'/>
+                        <button className='add' onClick={e => this.handleDelete(e, goal.id)}> x </button>
+                      </div>
+                    )  
+                  }
+                <input type='text' className='new-item' onChange={this.handleChange} value={this.state.new_goal}/> <button className='add' onClick={this.addGoal}> + </button>
+              </form>
+          </div>
+        </React.Fragment>
+      );
+    }
   }
   
   export default Goals;
--- src/Wins.js
+++ src/Wins.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
-function DashboardContainer() {
+class DashboardContainer extends React.Component {
+  
+  state = {
+    user_id: 1,
+    wins: ['']
+  }
+
+  componentDidMount() {
+    // get the wins already submitted for today
+  }
+
+  addRow = () => {
+    // add fetch here, adding each row should send fetch request to add the wins to the backend
+    // after the button clicked the plus sign should turn into an x to delete or a edit button
+
+  }
+  
+  render() {
     return (
       <React.Fragment>
         <h1 className="title">Wins</h1>
         <div className="container small">
-            <p className='small-description'>Use this space to share any wins from yesterday</p>
+            <p className='small-description'>Use this space to share your wins from yesterday</p>
             <form> 
-              <input type='text' className='new-item'/> <button className='add'> + </button>
+              <input type='text' className='new-item'/> <button className='add' onClick={this.addRow}> + </button>
             </form>
         </div>
       </React.Fragment>
     );
   }
+}
   
-  export default DashboardContainer;
+export default DashboardContainer;
   
