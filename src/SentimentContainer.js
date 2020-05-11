fe5e548a5508e5cd1d205dac2f1060e8c53e4e4c
merewelch214 <merewelch@gmail.com>
2020-05-11 19:23:14 -0400

    dates to sentiment chart

--- src/SentimentContainer.js
+++ src/SentimentContainer.js
@@ -4,17 +4,19 @@ import {Line} from 'react-chartjs-2';
 class JournalEntry extends React.Component {
 
   state = {
-    journal_data: {}
+    journal_data: {},
+    isLoaded: false
   }
 
   componentDidMount() {
     fetch(`http://localhost:3000/journal_entry_score_dates/1`)
     .then(resp=>resp.json())
-    .then(data=>this.setState({journal_data: data}))
+    .then(data=>this.setState({journal_data: data, isLoaded: true}))
   }
 
+  // get an array of dates to use as the labels for the x-axis.
   getDates = () => {
-    let datesArray = []
+    let datesArray = [];
     for (let x = 30; x >= 0 ; x--) {
       let date = new Date(); 
       date.setDate(date.getDate() - x);
@@ -22,12 +24,31 @@ class JournalEntry extends React.Component {
       let day = date.getDate()
       datesArray.push(`${month}-${day}`)
     }
-    return datesArray
+    return datesArray;
   }
 
+  // format the journal entry score and date data for the chart, using the t (t for time, or the x) axis and y axis
+  getData = (array) => {
+    let final_array = []
+    if (!!array) {
+      for (let i = 0; i < array.length; i++) {
+        let obj = {}; 
+        let time = new Date(array[i][0])
+        let month = time.getMonth()+1
+        let day = time.getDate()
+        obj['t'] = `${month}-${day}`;
+        obj['y'] = array[i][1];
+        final_array.push(obj);
+      }
+      console.log(final_array)
+    return final_array;
+    }
+  }
+
+
   render() {  
-    // const month_names_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     
+    // data inputs for chart 
     const data = {
       labels: this.getDates(), 
       options: {
@@ -69,17 +90,7 @@ class JournalEntry extends React.Component {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [{
-            t: new Date("2015-3-15 13:3").toLocaleString(),
-            y: 1
-          }, {
-            t: new Date("2015-3-25 13:2").toLocaleString(),
-            y: 10
-          }, {
-            t: new Date("2015-4-25 14:12").toLocaleString(),
-            y: 54
-          }
-        ]
+          data: this.state.isLoaded && this.getData(this.state.journal_data)
         }
       ]
     }
@@ -88,7 +99,7 @@ class JournalEntry extends React.Component {
           <div>
             <h1 className="title">Your sentiment over time</h1>
               <div className="container">
-                < Line data={data}/>
+                {this.state.isLoaded ? < Line data={data}/> : <div> Loading... </div>}
               </div>
           </div> 
       );
