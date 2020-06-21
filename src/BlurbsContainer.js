68dd7023738167e1a53d363b9f566e9ecee9a80e
merewelch214 <merewelch@gmail.com>
2020-06-21 19:19:49 -0400

    disable prior next when reach limit

--- src/BlurbsContainer.js
+++ src/BlurbsContainer.js
@@ -26,17 +26,20 @@ class BlurbsContainer extends React.Component {
         
         let current_entries = sorted_entries.slice(this.state.currentIndex, this.state.currentIndex + 3)
 
-        console.log(current_entries[0] === this.state.journal_entries[0])
-
+        console.log(current_entries[0] === sorted_entries[0])
+        console.log(current_entries[0])
+        console.log(this.state.journal_entries[0])
         return (
             <div>
                 {current_entries.map(journal_entry => < Blurb {...journal_entry} key={journal_entry.id} /> )}<br />
-                <button 
-                    disabled={current_entries[0] === this.state.journal_entries[0]} 
-                    onClick={this.previous}> Previous </button>
-                <button 
-                    disabled={current_entries[current_entries.length - 1] === this.state.journal_entries[this.state.journal_entries.length - 1]} 
-                    onClick={this.next}> Next </button>
+                <div className='prev-next-buttons'>
+                    <button 
+                        disabled={current_entries[0] === sorted_entries[0]} 
+                        onClick={this.previous}> Previous </button>
+                    <button 
+                        disabled={current_entries[current_entries.length - 1] === sorted_entries[sorted_entries.length - 1]} 
+                        onClick={this.next}> Next </button>
+                </div>
             </div>
         )
     }
