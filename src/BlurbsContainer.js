094d37dc22110370135bbe29b2efcf9885f10b98
merewelch214 <merewelch@gmail.com>
2020-06-21 19:11:42 -0400

    show 3 entries at a time

--- src/BlurbsContainer.js
+++ src/BlurbsContainer.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Blurb from './Blurb';
-// import InfiniteScroll from 'react-infinite-scroll-component';
-
 class BlurbsContainer extends React.Component {
     
     state = {
         journal_entries: [],
-        hasMore: true
+        hasMore: true,
+        currentIndex: 0
     }
 
     // fetch request to get all of journal entries once the component has mounted
@@ -16,43 +15,28 @@ class BlurbsContainer extends React.Component {
         .then(data => this.setState({journal_entries: data}))
     }
 
-    // to do:
-    fetchMoreData = () => {
-        if (this.state.journal_entires.length >= 500) {
-            this.setState({ hasMore: false });
-        // return;
-        }
+    next = () => {
+        this.setState((prevState) => ({currentIndex: prevState.currentIndex + 3}))
     }
+    
+    previous = () => this.setState((prevState) => ({currentIndex: prevState.currentIndex - 3}))
 
     render() {
         const sorted_entries = [...this.state.journal_entries].sort((a,b)=>b.id-a.id)
+        
+        let current_entries = sorted_entries.slice(this.state.currentIndex, this.state.currentIndex + 3)
+
+        console.log(current_entries[0] === this.state.journal_entries[0])
+
         return (
             <div>
-                {sorted_entries.map(journal_entry => < Blurb {...journal_entry} key={journal_entry.id} /> )}
-
-                
-                {/* <InfiniteScroll
-                    dataLength={this.state.journal_entries.length} //This is important field to render the next data
-                    next={this.fetchMoreData}
-                    hasMore={this.state.hasMore}
-                    loader={<h4>Loading...</h4>}
-                    endMessage={
-                        <p style={{textAlign: 'center'}}>
-                        <b>Yay! You have seen it all</b>
-                        </p>
-                    }
-                    // below props only if you need pull down functionality
-                    refreshFunction={this.refresh}
-                    pullDownToRefresh
-                    pullDownToRefreshContent={
-                        <h3 style={{textAlign: 'center'}}>&#8595; Pull down to refresh</h3>
-                    }
-                    releaseToRefreshContent={
-                        <h3 style={{textAlign: 'center'}}>&#8593; Release to refresh</h3>
-                    }>
-                    {sorted_entries.map(journal_entry => < Blurb {...journal_entry} key={journal_entry.id} /> )}
-leen                </InfiniteScroll>
-                 */}
+                {current_entries.map(journal_entry => < Blurb {...journal_entry} key={journal_entry.id} /> )}<br />
+                <button 
+                    disabled={current_entries[0] === this.state.journal_entries[0]} 
+                    onClick={this.previous}> Previous </button>
+                <button 
+                    disabled={current_entries[current_entries.length - 1] === this.state.journal_entries[this.state.journal_entries.length - 1]} 
+                    onClick={this.next}> Next </button>
             </div>
         )
     }
