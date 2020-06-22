f3d191b24599a9537c5294e91dced5d554216739
merewelch214 <merewelch@gmail.com>
2020-06-21 20:38:35 -0400

    fix date formatting in blurbs

--- src/BlurbsContainer.js
+++ src/BlurbsContainer.js
@@ -15,10 +15,12 @@ class BlurbsContainer extends React.Component {
         .then(data => this.setState({journal_entries: data}))
     }
 
+    // get next 3 items from state
     next = () => {
         this.setState((prevState) => ({currentIndex: prevState.currentIndex + 3}))
     }
     
+    // get prior 3 items from state
     previous = () => this.setState((prevState) => ({currentIndex: prevState.currentIndex - 3}))
 
     render() {
@@ -26,17 +28,16 @@ class BlurbsContainer extends React.Component {
         
         let current_entries = sorted_entries.slice(this.state.currentIndex, this.state.currentIndex + 3)
 
-        console.log(current_entries[0] === sorted_entries[0])
-        console.log(current_entries[0])
-        console.log(this.state.journal_entries[0])
         return (
             <div>
                 {current_entries.map(journal_entry => < Blurb {...journal_entry} key={journal_entry.id} /> )}<br />
                 <div className='prev-next-buttons'>
                     <button 
+                        // if first items match, there are no previous entries
                         disabled={current_entries[0] === sorted_entries[0]} 
                         onClick={this.previous}> Previous </button>
                     <button 
+                        // if last items match, there are no next entries
                         disabled={current_entries[current_entries.length - 1] === sorted_entries[sorted_entries.length - 1]} 
                         onClick={this.next}> Next </button>
                 </div>
