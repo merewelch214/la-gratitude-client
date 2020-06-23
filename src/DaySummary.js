3222ae3e3471a3dc158618019b5a9ea3c2e218ba
merewelch214 <merewelch@gmail.com>
2020-06-23 19:55:23 -0400

    day summary component

---
+++ src/DaySummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function DaySummary() {
+    
+    // this will be through the route user/:id/date and will show a full version of the journal entry along with goals and wins
+    
+    // fetch request to get journal entry, goals, wins. 
+
+    return (
+      <div className="container">
+          <div className='title'> IDK </div>
+      </div>
+    );
+  }
+  
+  export default DaySummary;
+  
